72e92480bd4c19d7a6a6a4d6daee3a85bad57129
Khanh Pham <105581919+QuyKhanh03@users.noreply.github.com>
2022-11-22 11:53:11 +0700

    fix detail and add also

--- controllers/c_product.php
+++ controllers/c_product.php
@@ -3,9 +3,13 @@ class c_product {
     public function product_detail() {
         if(isset($_GET["id"])) {
             require_once "models/m_product.php";
-            $id = $_GET["id"];
             $m_prd = new m_product();
-            $prd_detail = $m_prd->read_product_detail();
+            $id = $_GET["id"];
+            $prd_detail = $m_prd->read_product_detail($id);
+            foreach($prd_detail as $key => $item) {
+                $cate_id = $item->cate_id;
+                $also = $m_prd->also($cate_id);
+            }
         }
 
         // tạm thời chưa cần lấy dữ liệu ra, cứ view ra đc layout đã rồi tính
--- models/m_product.php
+++ models/m_product.php
@@ -3,10 +3,16 @@ include "database.php";
  
 class m_product extends database {
     // extends database là kế thừa từ database (là những hàm viết ở database.php)
-    public function read_product_detail() {
-        $id = $_GET["id"];
-        $sql = "select * from products where id = ?";
+    public function read_product_detail($id) {
+        $sql = "select * from products,categories where products.id = '$id' and products.cate_id = categories.cate_id";
         $this->setQuery($sql);
-        return $this->loadRow(array($id));
+        return $this->loadAllRows();
+    }
+    public function also($id_cate) {
+        $id = $_GET["id"];
+        $sql = "select * from products,categories where
+        products.cate_id = '$id_cate' and categories.cate_id = '$id_cate' and products.id <> '$id'";
+        $this->setQuery($sql);
+        return $this->loadAllRows();
     }
 }
