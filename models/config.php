254b1d17706815f223095816e2a2024aa0fc9496
dat chu <139096474+datcvph31745@users.noreply.github.com>
2023-12-21 20:30:35 +0700

    Delete models directory

--- models/config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-define("DB_HOST","localhost");
-define("DB_NAME","shopfashion");
-define("DB_USER","root");
-define("DB_PWD","");
--- models/m_home.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include "database.php";
-class m_home extends database {
-    public function loadAllPrd() {
-        $sql = "select * from products";
-        $this->setQuery($sql);
-        return $this->loadAllRows();
-    }
-}
